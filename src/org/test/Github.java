5768933d53f35cb3b32837e375b28447c773c62c
haran2231 <haran2231@gmail.com>
2022-04-14 23:10:13 +0530

    first commit

---
+++ src/org/test/Github.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Github {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hai Hari");
+		
+		System.out.println("Have a nice day");
+	}
+
+}
